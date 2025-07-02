70e95c545a7e25c20aa29c00529693912f011d2c
hugodiana <hugo-hdiana@hotmail.com>
2025-07-02 15:12:45 -0300

    Configura backend para usar PostgreSQL em produção

--- backend/config.py
+++ backend/config.py
@@ -3,11 +3,15 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma-chave-secreta-muito-dificil-de-adivinhar'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma-chave-secreta-muito-dificil'
+
+    # LÓGICA ATUALIZADA:
+    # 1. Tenta pegar a URL do banco de dados da variável de ambiente 'DATABASE_URL' (que vamos configurar na Render).
+    # 2. Se não encontrar, usa o banco de dados SQLite local como antes.
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'meuapp_v3.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
     SESSION_COOKIE_SECURE = True
